fdeb938ac3e4c4a8519a6bf2ac0080370b571fd2
octaviedros <octavie.droste@t-online.de>
2024-12-13 14:16:10 +0100

    Add PostgreSQL dependency and create schema update script

---
+++ create_update_tables.py
@@ -0,0 +1,11 @@
+# update_schema.py
+import os
+from config import username, password, db
+
+uri = f"host='localhost' dbname='{db}' user='{username}' password='{password}'"
+
+if __name__ == "__main__":
+    sql_schema_dir = 'schema'
+    command = f"pg-schema-diff apply --dsn 'postgres://{username}:{password}@localhost:5432/{db}' --schema-dir {sql_schema_dir}"
+    os.system(command)
+
