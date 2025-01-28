1a15694de8b5c63d8d73488a25aa165e784a6b7b
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2025-01-28 16:49:23 +0100

    ➕ add omymodels and polars

--- create_update_tables.py
+++ create_update_tables.py
@@ -6,6 +6,6 @@ uri = f"host='localhost' dbname='{db}' user='{username}' password='{password}'"
 
 if __name__ == "__main__":
     sql_schema_dir = 'schema'
-    command = f"pg-schema-diff apply --dsn 'postgres://{username}:{password}@localhost:5432/{db}' --schema-dir {sql_schema_dir} --allow-hazards DELETES_DATA"
+    command = f"pg-schema-diff apply --dsn 'postgres://{username}:{password}@localhost:5432/{db}' --schema-dir {sql_schema_dir} --allow-hazards DELETES_DATA ACQUIRES_ACCESS_EXCLUSIVE_LOCK IMPACTS_DATABASE_PERFORMANCE"
     os.system(command)
     os.system("uv run omm schema/agribenchmark_schema.sql -m sqlmodel -t src/models.py")
